665a112e085f6beb221bb747135c6093bf540c6e
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2024-06-04 00:15:06 +0200

    add gdb-splitmind

--- flake.nix
+++ flake.nix
@@ -1,50 +1,47 @@
 {
   description = "CTF Flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  outputs = { self, nixpkgs }:
-  let
+  outputs = { self, nixpkgs }@inputs:
+    let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-      devShells.${system}.default =
-        pkgs.mkShell {
-            buildInputs = with pkgs; [
-                gdb
-                ltrace
-                nasm
-                one_gadget
-                pwndbg
-                pwninit
-                ropgadget
-                socat
-                strace
-                curl
-                qemu
-                musl
-                python311Packages.angr
-                python311Packages.claripy
-                python311Packages.gmpy2
-                python311Packages.ipython
-                python311Packages.numpy
-                python311Packages.pillow
-                python311Packages.pwntools
-                python311Packages.pycryptodome
-                python311Packages.pyperclip
-                python311Packages.requests
-                python311Packages.scapy
-                python311Packages.scipy
-                python311Packages.seccomp
-                python311Packages.tqdm
-                python311Packages.z3
-                python311Packages.ropper
-                (import ./upload-kernel-exploit.nix { inherit pkgs; })
-            ];
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gdb
+          ltrace
+          nasm
+          one_gadget
+          pwndbg
+          pwninit
+          ropgadget
+          socat
+          strace
+          curl
+          qemu
+          musl
+          python311Packages.angr
+          python311Packages.claripy
+          python311Packages.gmpy2
+          python311Packages.ipython
+          python311Packages.numpy
+          python311Packages.pillow
+          python311Packages.pwntools
+          python311Packages.pycryptodome
+          python311Packages.pyperclip
+          python311Packages.requests
+          python311Packages.scapy
+          python311Packages.scipy
+          python311Packages.seccomp
+          python311Packages.tqdm
+          python311Packages.z3
+          python311Packages.ropper
+          (import ./upload-kernel-exploit.nix { inherit pkgs; })
+          (import ./gdb-splitmind.nix { inherit pkgs stdenv; })
+        ];
 
-        };
       };
-  }
+    };
+}
---
+++ gdb-splitmind.nix
@@ -0,0 +1,45 @@
+{ pkgs, stdenv, gdb ? pkgs.gdb, pwndbg ? pkgs.pwndbg }:
+let
+  splitmindSrc = pkgs.fetchFromGitHub {
+    owner = "jerdna-regeiz";
+    repo = "splitmind";
+    rev = "master";
+    sha256 = "RsPU8tmBjQevAdGU5WgPRsxvPe5gZ+TU07Ol4oYCWaU=";
+  };
+in stdenv.mkDerivation {
+  pname = "gdb-splitmind";
+  version = "1.0";
+
+  src = splitmindSrc;
+
+  buildInputs = [ pkgs.python3Packages.setuptools pkgs.makeWrapper ];
+
+  doCheck = false;
+  doInstallCheck = false;
+
+  gdbinit = pkgs.writeText "$out/share/gdbinit" ''
+    set follow-fork-mode parent
+    set show-flags on
+    python
+    import splitmind
+    (splitmind.Mind()
+     .right(display="regs")
+     .below(of="regs", display="stack")
+    ).build()
+    end
+    set context-code-lines 10
+    set context-source-code-lines 5
+    set context-stack-lines 12
+    set context-sections  "args code disasm stack backtrace"
+  '';
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/splitmind
+    cp -r * $out/share/splitmind
+    echo "source $out/share/splitmind/gdbinit.py" > $out/share/gdbinit
+    cat $gdbinit >> $out/share/gdbinit
+    makeWrapper ${pwndbg}/bin/pwndbg $out/bin/g \
+    --add-flags "--command=$out/share/gdbinit"
+    runHook postInstall
+  '';
+}
